60e6835e1d45641a0e07f1b6b6f80b53eef325de
ScadeBlock <89845150+ScadeBlock@users.noreply.github.com>
2024-05-04 20:09:45 +0700

    Create origin-file.py

---
+++ demo/origin-file.py
@@ -0,0 +1,9 @@
+# PythonNoMercy demo
+print("This is a script for testing PythoNoMercy")
+print("Two Sum")
+a = input("Number 1 > ")
+b = input("Number 2 > ")
+try:
+    print("Sum: "+str(int(a)+int(b)))
+except:
+    print("Invailid number")
